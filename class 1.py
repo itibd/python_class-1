3039813efa675bb2692482ccf205c37357f665ca
“itibd” <rahulitibd@gmail.com>
2022-05-16 16:47:16 +0600

    class 1.py

---
+++ class 1.py
@@ -0,0 +1,16 @@
+#a=15 #a is variable and 15 is value
+#b=325
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(b/a)
+#print(b%a)
+#d=int(input("inter a:"))
+#r=d/2
+#pi=22/7
+#p=2*pi*r
+#print(p)
+a=int(input())
+b=int(input())
+c=a%b
+print(c)
